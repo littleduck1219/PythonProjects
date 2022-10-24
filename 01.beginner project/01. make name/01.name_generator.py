bb32e448d083bc3865bf927cc8265cfc542349ce
littleduck1219 <littleduck1219@gmail.com>
2022-10-24 23:08:58 +0900

    01

---
+++ 01.beginner project/01. make name/01.name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the name generator") 
+city = input("which city did you grew up in?") # input city name
+pet = input("what's your pet's name?") # input pet's name
+print(city + " " + pet) # combine city, pet
