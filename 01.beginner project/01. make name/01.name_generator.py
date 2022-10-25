3e422bd644353c2d846ad35d4743711a69b37ad3
littleduck1219 <littleduck1219@gmail.com>
2022-10-25 11:36:46 +0900

    update

--- 01.beginner project/01. make name/01.name_generator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Life in Weeks") 
-first_name = input("What is your first name?") # input first name
-last_name = input("What is your last name?") # input last name
-age = input("What is your current age?") # input your age
-
-left = 90 - int(age) # this assuming your life is up to 90
-print(f" {first_name} {last_name}\nyou have {left*365} days, {left*52} weeks, and {left*12} months left.")
