fe09750962524122ac1be0de0afc405b4ff05c0d
littleduck1219 <littleduck1219@gmail.com>
2022-10-25 10:28:11 +0900

    01

--- 01.beginner project/01. make name/01.name_generator.py
+++ 01.beginner project/01. make name/01.name_generator.py
@@ -1,4 +1,7 @@
-print("Welcome to the name generator") 
-city = input("which city did you grew up in?") # input city name
-pet = input("what's your pet's name?") # input pet's name
-print(city + " " + pet) # combine city, pet
+print("Welcome to the Life in Weeks") 
+first_name = input("What is your first name?") # input first name
+last_name = input("What is your last name?") # input last name
+age = input("What is your current age?") # input your age
+
+left = 90 - int(age) # this assuming your life is up to 90
+print(f" {first_name} {last_name}\nyou have {left*365} days, {left*52} weeks, and {left*12} months left.")
